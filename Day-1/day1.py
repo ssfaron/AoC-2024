690a3694688dd60028291471e7ed85a9a351fd01
ssfaron <slsmallw.asu@gmail.com>
2024-11-30 19:24:54 -0600

    first commit

---
+++ Day-1/day1.py
@@ -0,0 +1,14 @@
+######################################################
+### Author: Sarah Faron
+### Advent of Code 2024
+### Day 1: 11/30/2024
+######################################################
+
+### Part 1 ###
+input = "input.txt"
+
+# Read in the input file
+with open(input) as f:
+    lines = f.readlines()
+
+    #for line in lines:
